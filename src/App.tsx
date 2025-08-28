1aa47af7cfac52741e4e21ae14b42ed0afc2ab08
Renarc28 <huapayarenzo2@gmail.com>
2025-08-28 11:34:40 -0500

    parte 3

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+return (
+<div style={{ textAlign: "center", marginTop: "50px" }}>
+<h1>🚀 Mi Primera App con React</h1>
+<p>Desarrollo Web Moderno en acción</p>
+</div>
+);
+}
+
+export default App
